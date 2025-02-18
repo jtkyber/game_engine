e39714a22614ca9b1d6db08730984909f7e4aec9
jtkyber <43868043+jtkyber@users.noreply.github.com>
2025-02-17 21:26:04 -0500

    Fixes

--- src/control/app.ts
+++ src/control/app.ts
@@ -7,7 +7,7 @@ import { eulerFromQuat } from '../utils/math';
 import { newStatus, quatFromTime } from '../utils/misc';
 import BindGroupLayouts from '../view/bindGroupLayouts';
 import GLTFImage from '../view/gltf/image';
-import GTLFLoader, { models, nodes } from '../view/gltf/loader';
+import GTLFLoader, { animations, models, nodes } from '../view/gltf/loader';
 import GLTFNode from '../view/gltf/node';
 import Renderer from '../view/renderer';
 import { Skybox } from '../view/skybox';
